7737560a26e49b5b70a8b019544f0acd80c147ad
Ajinkya Ajay Pundle <ratnadeeppundle@gmail.com>
2024-12-03 18:57:54 +0530

    Add files via upload

---
+++ Ecommerce Website/app.js
@@ -0,0 +1,13 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const closed = document.getElementById("closed");
+if(bar){
+    bar.addEventListener("click",()=>{
+         nav.classList.add("active");
+    });
+} ;
+if(closed){
+    closed.addEventListener("click",()=>{
+         nav.classList.remove("active");
+    })
+} 
